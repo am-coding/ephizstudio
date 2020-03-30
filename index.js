2f1b654acba5449fe62a70aeb387b5103fdafd57
am-coding <60037787+am-coding@users.noreply.github.com>
2020-03-30 11:37:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
